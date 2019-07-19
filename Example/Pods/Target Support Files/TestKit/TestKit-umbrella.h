6c17392591710c31a82969c6d76ffd6a71a479c0
常远 <changyuan@yingu.com>
2019-07-19 16:44:43 +0800

    涉及金额显示添加,分割符的扩展类

---
+++ Example/Pods/Target Support Files/TestKit/TestKit-umbrella.h
@@ -0,0 +1,18 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NSString+MoneyNumberStyle.h"
+#import "UILabel+Extension.h"
+
+FOUNDATION_EXPORT double TestKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char TestKitVersionString[];
+
